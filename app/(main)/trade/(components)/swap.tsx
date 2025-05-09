a6aa24b01085eb00cc0e61ad558a096fa89b9f74
Lucas Tenório <59018759+lucastnr@users.noreply.github.com>
2025-05-09 18:51:40 -0300

    Refactor provider initialization to use useMemo

--- app/(main)/trade/(components)/swap.tsx
+++ app/(main)/trade/(components)/swap.tsx
@@ -1,10 +1,10 @@
 "use client";
-import { useWalletClient } from 'wagmi'
-import { useEffect, useState } from 'react';
-import { Web3Provider } from '@ethersproject/providers'
+import { Web3Provider } from "@ethersproject/providers";
 import { darkTheme } from "@uniswap/widgets";
 import "@uniswap/widgets/fonts.css";
 import dynamic from "next/dynamic";
+import { useMemo } from "react";
+import { useWalletClient } from "wagmi";
 
 const SwapWidget = dynamic(
   async () => {
@@ -13,19 +13,13 @@ const SwapWidget = dynamic(
   { ssr: false },
 );
 
-
 export const Swap = () => {
-  const { data: walletClient } = useWalletClient()
-  const [provider, setProvider] = useState(null)
+  const { data: walletClient } = useWalletClient();
 
-  useEffect(() => {
-    async function loadProvider() {
-      if (!walletClient) return
-      const provider = new Web3Provider(walletClient.transport, walletClient.chain.id)
-      setProvider(provider)
-    }
-    loadProvider()
-  }, [walletClient])
+  const provider = useMemo(() => {
+    if (!walletClient) return;
+    return new Web3Provider(walletClient.transport, walletClient.chain.id);
+  }, [walletClient]);
 
   return (
     <SwapWidget
@@ -44,7 +38,7 @@ export const Swap = () => {
         onAccent: "#27272A",
         accent: "#FFFFB7",
         interactive: "#136b3b",
-        onInteractive: "white",
+        onInteractive: "#FFFFFF",
         accentSoft: "#136b3b",
 
         secondary: "#989898",
