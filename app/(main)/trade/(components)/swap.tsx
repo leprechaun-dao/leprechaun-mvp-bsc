1ef80c9ae53dbb84c947843963b2c7e4b949d30d
Diego Jose <diegoxter1006@gmail.com>
2025-05-09 15:22:59 -0300

    wagmi provider/hook on Uniswap's Swap component

--- app/(main)/trade/(components)/swap.tsx
+++ app/(main)/trade/(components)/swap.tsx
@@ -1,4 +1,7 @@
 "use client";
+import { useWalletClient } from 'wagmi'
+import { useEffect, useState } from 'react';
+import { Web3Provider } from '@ethersproject/providers'
 import { darkTheme } from "@uniswap/widgets";
 import "@uniswap/widgets/fonts.css";
 import dynamic from "next/dynamic";
@@ -10,9 +13,24 @@ const SwapWidget = dynamic(
   { ssr: false },
 );
 
+
 export const Swap = () => {
+  const { data: walletClient } = useWalletClient()
+  const [provider, setProvider] = useState(null)
+
+  useEffect(() => {
+    async function loadProvider() {
+      if (!walletClient) return
+      const provider = new Web3Provider(walletClient.transport, walletClient.chain.id)
+      setProvider(provider)
+    }
+    loadProvider()
+  }, [walletClient])
+
   return (
     <SwapWidget
+      hideConnectionUI={true}
+      provider={provider}
       className="Uniswap"
       // TODO: Replace with our token list
       tokenList={"https://ipfs.io/ipns/tokens.uniswap.org"}
