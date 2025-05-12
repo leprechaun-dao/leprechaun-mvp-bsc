888f3df0103a6b636a595fce973d69aae30e2000
Lucas Tenório <59018759+lucastnr@users.noreply.github.com>
2025-05-12 20:46:25 -0300

    Add dialogRef to Swap component for dynamic dialog handling

--- app/(main)/trade/(components)/swap.tsx
+++ app/(main)/trade/(components)/swap.tsx
@@ -2,6 +2,7 @@
 import { darkTheme } from "@uniswap/widgets";
 import "@uniswap/widgets/fonts.css";
 import dynamic from "next/dynamic";
+import { useRef } from "react";
 
 const SwapWidget = dynamic(
   async () => {
@@ -11,28 +12,45 @@ const SwapWidget = dynamic(
 );
 
 export const Swap = () => {
+  const dialogRef = useRef<HTMLDivElement>(null);
   return (
-    <SwapWidget
-      className="Uniswap"
-      // TODO: Replace with our token list
-      tokenList={"https://ipfs.io/ipns/tokens.uniswap.org"}
-      theme={{
-        ...darkTheme,
-        container: "#0E0E12",
-        module: "#18181B",
-        outline: "#27272A",
-        networkDefaultShadow: "#B4871233",
+    <div>
+      <div
+        className="absolute inset-0 m-10 pointer-events-none [&>*]:pointer-events-auto"
+        ref={dialogRef}
+      ></div>
+      <style>
+        {`
+          // This is hack to fix a weird issue with the Uniswap widget
+          .TokenOptions__OnHover-sc-xx1k3q-2 {
+            display: none;
+          }
+        `}
+      </style>
+      <SwapWidget
+        className="Uniswap"
+        // TODO: Replace with our token list
+        tokenList={"https://ipfs.io/ipns/tokens.uniswap.org"}
+        brandedFooter={false}
+        dialog={dialogRef.current}
+        theme={{
+          ...darkTheme,
+          container: "#0E0E12",
+          module: "#18181B",
+          outline: "#27272A",
+          networkDefaultShadow: "#B4871233",
 
-        onAccent: "#27272A",
-        accent: "#FFFFB7",
-        interactive: "#136b3b",
-        onInteractive: "white",
-        accentSoft: "#136b3b",
+          onAccent: "#27272A",
+          accent: "#FFFFB7",
+          interactive: "#136b3b",
+          onInteractive: "white",
+          accentSoft: "#136b3b",
 
-        secondary: "#989898",
-        primary: "#EEEEEE",
-        fontFamily: "var(--font-inter)",
-      }}
-    />
+          secondary: "#989898",
+          primary: "#EEEEEE",
+          fontFamily: "var(--font-inter)",
+        }}
+      />
+    </div>
   );
 };
