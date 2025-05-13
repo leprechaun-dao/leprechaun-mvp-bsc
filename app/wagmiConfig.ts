00e5ef956e8345aae200f2da704c63e01d68b385
Diego Jose <diegoxter1006@gmail.com>
2025-05-13 16:09:26 -0300

    update back to base

--- app/wagmiConfig.ts
+++ app/wagmiConfig.ts
@@ -1,13 +1,16 @@
 import { createConfig, http } from "wagmi";
-import { arbitrum } from "wagmi/chains";
+// import { arbitrum } from "wagmi/chains";
+import { base } from "wagmi/chains";
 import { metaMask } from "wagmi/connectors";
 
 export const wagmiConfig = createConfig({
   // TODO change this to base once the new deployments are up
-  chains: [arbitrum],
+  // chains: [arbitrum],
+  chains: [base],
   connectors: [metaMask()],
   ssr: true,
   transports: {
-    [arbitrum.id]: http(),
+    // [arbitrum.id]: http(),
+    [base.id]: http(),
   },
 });
--- utils/constants.ts
+++ utils/constants.ts
@@ -1,9 +1,9 @@
 // TODO make these wagmi contracts?
-import {ERC20ABI} from "@/utils/abis/ERC20";
-import {LeprechaunFactoryABI} from "@/utils/abis/LeprechaunFactory.js";
-import {OracleInterfaceABI} from "@/utils/abis/OracleInterface";
-import {PositionManagerABI} from "@/utils/abis/PositionManager";
-import {SyntheticAssetABI} from "@/utils/abis/SyntheticAsset";
+import { ERC20ABI } from "@/utils/abis/ERC20";
+import { LeprechaunFactoryABI } from "@/utils/abis/LeprechaunFactory.js";
+import { OracleInterfaceABI } from "@/utils/abis/OracleInterface";
+import { PositionManagerABI } from "@/utils/abis/PositionManager";
+import { SyntheticAssetABI } from "@/utils/abis/SyntheticAsset";
 
 export const rpcUrl =
   "https://base-mainnet.infura.io/v3/" + process.env.NEXT_PUBLIC_INFURA_KEY;
@@ -13,14 +13,14 @@ export const icoAddress = "0x99ECa4843D85b73b4EEA8e987B63B77763DAadFd";
 export const lprAddress = "0x7AD3b12d91e81ebd218f9acF55E2A699d7f68A33";
 
 // TODO change/take out these to base once the new deployments are up
-export const LeprechaunFactoryAddress = "0x5c7FF36e0BB492c81d85e501C8ce9a418618A4eD"
-export const PositionManagerAddress = "0xA202BBa404427dEa715D0ca424FB7dA337fF3a46"
-export const OracleInterfaceAddress = "0x3979eeBA732A0d8B422557f489E381e6ee2DD1F8"
+export const LeprechaunFactoryAddress = "0x364A6127A8b425b6857f4962412b0664D257BDD5"
+export const PositionManagerAddress = "0x401d1cD4D0ff1113458339065Cf9a1f2e8425afb"
+export const OracleInterfaceAddress = "0xBc2e651eD3566c6dF862815Ed05b99eFb9bC0255"
 
-export const sDOWAddress = "0x9E623E30bddA40464945cf14777CA990BE2Ba984"
+export const sDOWAddress = "0xD14F0B478F993967240Aa5995eb2b1Ca6810969a"
 
-export const mUSDCAddress = "0x26bb5E0E1b93440720cebFCdD94CaA7B515af1cf"
-export const mWETHAddress = "0x9FB8bc690C3Dcf32464062f27658A42C87F25C26"
-export const mWBTCAddress = "0xE157a88bDaFf6487408131b8369CaaE56691E562"
+export const mUSDCAddress = "0x39510c9f9E577c65b9184582745117341e7bdD73"
+export const mWETHAddress = "0x95539ce7555F53dACF3a79Ff760C06e5B4e310c3"
+export const mWBTCAddress = "0x1DBf5683c73E0D0A0e20AfC76F924e08E95637F7"
 
 export { ERC20ABI, PositionManagerABI, OracleInterfaceABI, SyntheticAssetABI, LeprechaunFactoryABI }
