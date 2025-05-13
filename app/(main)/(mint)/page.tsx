e24c9646b9051d8303606c1987f0cd5e035b3ef8
Lucas Tenório <59018759+lucastnr@users.noreply.github.com>
2025-05-12 21:53:32 -0300

    Add form components and integrate CurrencyInput
    - Implemented reusable form components (Form, FormField, FormItem, etc.)
    - Added CurrencyInput component with formatting logic
    - Updated mint page to use new form and input components
    - Installed react-hook-form and zod for form handling and validation
    - Updated dependencies in package.json and yarn.lock

--- app/(main)/(mint)/page.tsx
+++ app/(main)/(mint)/page.tsx
@@ -1,10 +1,60 @@
+"use client";
+import { CurrencyInput } from "@/components/CurrencyInput";
 import { Header } from "@/components/layout/header";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
 
 export default function Home() {
+  const form = useForm();
+
+  const minted = form.watch("minted");
+
+  console.log("minted", minted);
+
   return (
-    <div>
+    <div className="flex flex-col min-h-screen w-full">
       <Header activeRoute="mint" />
-      <main></main>
+      <main className="flex flex-1 items-center justify-center mb-[20vh] px-6">
+        <Card className="w-lg">
+          <CardContent>
+            <div className="flex flex-col gap-2 [&_.input]:h-20 [&_.input]:text-right [&_.input]:text-lg">
+              <Form {...form}>
+                <FormField
+                  control={form.control}
+                  name="colateral"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Collateral</FormLabel>
+                      <FormControl>
+                        <CurrencyInput className="input" {...field} />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="minted"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Minted</FormLabel>
+                      <FormControl>
+                        <CurrencyInput className="input" {...field} />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </Form>
+            </div>
+          </CardContent>
+        </Card>
+      </main>
     </div>
   );
 }
