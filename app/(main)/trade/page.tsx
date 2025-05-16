753a82296ec3be01dd466811417b62912f6745e4
Lucas Tenório <59018759+lucastnr@users.noreply.github.com>
2025-05-16 19:17:41 -0300

    Remove uniswap widget

--- app/(main)/trade/page.tsx
+++ app/(main)/trade/page.tsx
@@ -1,16 +1,13 @@
 "use client";
 
 import { Header } from "@/components/layout/header";
-import { Swap } from "./(components)/swap";
 
 export default function Trade() {
   return (
     <div className="flex flex-col min-h-screen w-full">
       <Header activeRoute="trade" />
       <main className="flex flex-1 mb-[20vh] px-6">
-        <div className="flex w-full items-center justify-center">
-          <Swap />
-        </div>
+        <div className="flex w-full items-center justify-center"></div>
       </main>
     </div>
   );
